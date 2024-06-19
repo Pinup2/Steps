dfcc037757f60692ee177f9f5429f160ca9d0d9a
Pinup2 <anghelina.naida@gmail.com>
2024-06-19 09:24:35 +0300

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
-function App() {
+export default function App() {
+  //add piece of state
+  // const array = useState(1);
+  //new piece of state
+  const [isOpen, setIsOpen] = useState(true);
+  // console.log(array);
+  //results in array, position One is with argument (1) and the second one is function which will
+  //defining useState variable
+  const [step, setStep] = useState(1);
+
+  //defining functions
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // entity &times; gives cross symbol
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            {/* array is zero based so we start with 1-1 =0*/}
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              onClick={handlePrevious}
+              style={{ background: "violet", color: "white" }}
+              className="previous"
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              className="next"
+              style={{ background: "violet", color: "white" }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-export default App;
